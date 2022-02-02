4a84a990b095ac5eefd558847f1a45f141fd6ba7
Muhammad Tahsin Amin <muhammadtahsinamin@gmail.com>
2022-02-02 13:05:45 +0600

    Use Axios for API call

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
+import MovieListing from "../components/MovieListing";
+import movieApi from "../common/apis/movieApi";
+import { APIKey } from "../common/apis/MovieApiKey";
 
-export default function Home() {
-  return <div>Home</div>;
-}
+const Home = () => {
+  useEffect(() => {
+    const movieText = "Harry";
+    const fetchMovies = async () => {
+      const response = await movieApi
+        .get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
+        .catch((err) => {
+          console.log(err);
+        });
+      console.log("Thge response from the api", response);
+    };
+    fetchMovies();
+  }, []);
+  return (
+    <div>
+      <div className="banner-img"></div>
+      <MovieListing />
+    </div>
+  );
+};
+
+export default Home;
