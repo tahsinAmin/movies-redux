7cabc73e56eefaef4f8a48721176f4d685bbbc8e
Muhammad Tahsin Amin <muhammadtahsinamin@gmail.com>
2022-02-02 19:09:34 +0600

    Little edit

--- src/components/MovieDetail.js
+++ src/components/MovieDetail.js
@@ -5,14 +5,63 @@ import { fetchAsyncMovieOrShowDetail } from "../features/movies/movieSlice";
 import { getSelectedMovieOrShow } from "../features/movies/movieSlice";
 
 export default function MovieDetail() {
-  const detail = useSelector(getSelectedMovieOrShow);
+  const data = useSelector(getSelectedMovieOrShow);
   const { imdbID } = useParams();
   const dispatch = useDispatch();
-  console.log(detail);
 
   useEffect(() => {
     dispatch(fetchAsyncMovieOrShowDetail(imdbID));
   }, [dispatch, imdbID]); // we want to run thew useEffect whenever we change the id
 
-  return <div>MovieDetail</div>;
+  return (
+    <div className="movie-section text-white flex justify-evenly px-[40px] py-0 font-semibold">
+      <div className="section-left">
+        <div className="movie-title">{data.Title}</div>
+        <div className="movie-rating pl-[3px] mt-[20px] text-indigo-300 flex space-x-4">
+          <span className="text-xl">
+            IMDB Rating <i className="fa fa-star text-orange-300"></i> :{" "}
+            {data.imdbRating}
+          </span>
+          <span className="text-xl">
+            IMDB Votes <i className="fa fa-thumbs-up text-white"></i> :{" "}
+            {data.imdbVotes}
+          </span>
+          <span className="text-xl">
+            Runtime <i className="fa fa-film text-gray-300"></i> :{" "}
+            {data.Runtime}
+          </span>
+          <span className="text-xl">
+            Year <i className="fa fa-calendar text-orange-300"></i> :{" "}
+            {data.Year}
+          </span>
+        </div>
+        <div className="movie-plot mt-[20px] leading-7 mb-10">{data.Plot}</div>
+        <div className="movie-info">
+          <div>
+            <span className=" text-indigo-300">Director: </span>
+            <span>{data.Director}</span>
+          </div>
+          <div>
+            <span className=" text-indigo-300">Stars: </span>
+            <span>{data.Actors}</span>
+          </div>
+          <div>
+            <span className=" text-indigo-300">Genres: </span>
+            <span>{data.Genre}</span>
+          </div>
+          <div>
+            <span className=" text-indigo-300">Languages: </span>
+            <span>{data.Language}</span>
+          </div>
+          <div>
+            <span className=" text-indigo-300">Awards: </span>
+            <span>{data.Awards}</span>
+          </div>
+        </div>
+      </div>
+      <div className="section-right ml-[30px]">
+        <img src={data.Poster} alt={data.Title} />
+      </div>
+    </div>
+  );
 }
